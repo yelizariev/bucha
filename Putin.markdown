d09cf0c248e66bb492576f4031f9d22b2be61fa1
Ivan Kropotkin <help@itpp.dev>
2024-03-02 09:56:47 +0100

    Лёлик, только без рук!

--- Putin.markdown
+++ Putin.markdown
@@ -3,6 +3,7 @@ layout: Adam
 portal: https://shabbat.lamourism.com/kolobok/%D0%A6%D0%BE%D0%B9%D0%96%D0%AB%D0%92/%D0%9F%D1%83%D1%82%D0%B8%D0%BD%D0%92%D0%9E%D0%A0/scene.gltf
 takeoff: "-0.14, 1.65, -3.76"
 landing: "0, 1.32, -2.58"
+REVERT_BEFORE_COMMIT: 666
+ShabbatTV: "0, 2, 10"
+ShabbatMP4: "https://bucha.lamourism.com/%D0%9A%D0%B0%D1%80%D1%82%D1%8B-%D0%B4%D0%B5%D0%BD%D1%8C%D0%B3%D0%B8-%D0%B4%D0%B2%D0%B0--%D1%81%D1%82%D0%B2%D0%BE%D0%BB%D0%B0.mp4"
 ---
-
-😍🧕🔥🔥🔥💋
---
+++ assets/js/Adam.js
@@ -0,0 +1,48 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+// Create a scene
+const scene = new THREE.Scene();
+
+/* And God said, “Let there be light,” and there was light. God saw that
+   the light was good, and he separated the light from the darkness. God
+   called the light “day,” and the darkness he called “night.” And there was
+   evening, and there was morning—the first day. */
+const light = new THREE.AmbientLight( 0xffffff );
+scene.add( light );
+
+// Create a camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+// Create a renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+
+// Create controls
+const controls = new OrbitControls( camera, renderer.domElement );
+
+
+
+function ShabbatTV() {
+    // Play Video
+    const exodus = document.getElementById( 'exodus' );
+    exodus.play();
+    exodus.addEventListener( 'play', function () {
+        // this.currentTime = 3;
+    });
+
+    const texture = new THREE.VideoTexture(exodus);
+    texture.minFilter = THREE.LinearFilter;
+    texture.magFilter = THREE.LinearFilter;
+    //texture.format = THREE.RGBAFormat;
+
+    const geometry = new THREE.PlaneGeometry(5, 2.8);
+    const material = new THREE.MeshBasicMaterial({ map: texture, side: THREE.DoubleSide });
+    screen = new THREE.Mesh(geometry, material);
+    scene.add(screen);
+    return screen;
+}
+
+export {scene, camera, controls, ShabbatTV, renderer}
