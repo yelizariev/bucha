e38d00bab50cef83c4e616ae37c626231c2dcece
Ivan Kropotkin <help@itpp.dev>
2024-03-21 20:09:22 +0100

    WIP

---
+++ Brodsky 23.markdown
@@ -0,0 +1,20 @@
+---
+layout: Adam3
+portal: assets/XXX/Brodsky/scene.gltf
+takeoff: "1.64, 1.44, 0.91"
+landing: "0.76, 1.42, 0.1"
+
+Skazka: https://moses.lamourism.com/porn/shabbat.mp4
+Kolobok: https://moses.lamourism.com/porn/shabbat.mp4
+Narnia: 
+ - https://moses.lamourism.com/mossad/follow-the-white-rabbit.jpg
+ - https://moses.lamourism.com/mossad/masada.jpg
+ - https://perestroika-2.com/images/witches.jpg
+ - https://moses.lamourism.com/mossad/masada.jpg
+Reminiscence:
+ - https://thepiratecircus.com/%F0%9F%87%B5%F0%9F%87%A6/13.jpg
+ - https://thepiratecircus.com/%F0%9F%87%B5%F0%9F%87%A6/7.jpg
+ - https://thepiratecircus.com/%F0%9F%87%B5%F0%9F%87%A6/23.jpg
+ - https://thepiratecircus.com/%F0%9F%87%B5%F0%9F%87%A6/2.jpg
+ - https://thepiratecircus.com/%F0%9F%87%B5%F0%9F%87%A6/1.jpg
+---
---
+++ assets/js/Adam3.js
@@ -0,0 +1,183 @@
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import * as THREE from 'three';
+//import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
+//import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js';
+
+const SETTINGS = {
+    "Skazka": {
+        "video_id": "teremok",
+        "source_id": "skazka",
+        "height": 1.6,
+        "position": [0, 1.87, -2.33],
+        "rotation": {
+            "x": 0,
+            "y": 0,
+        },
+        "material": function(texture){
+            return new THREE.MeshBasicMaterial({
+                map: texture,
+                side: THREE.FrontSide,
+            });
+        },
+    },
+    "Kolobok": {
+        "video_id": "moon",
+        "source_id": "USA",
+        "height": 19.5,
+        "position": [-1, -3.4552, -0.6],
+        "rotation": {
+            "x": Math.PI / 2,
+            "y": 0,
+            "z": 0,
+        },
+        "material": function(texture){
+            // TODO fisheye effect, e.g. https://stackoverflow.com/questions/13360625/three-js-fisheye-effect
+            return new THREE.MeshBasicMaterial({
+                map: texture,
+                side: THREE.FrontSide,
+            });
+        },
+    },
+    "Narnia": [
+        {
+            "img_id": "Narnia0",
+            "width": 111,
+            "height": 111,
+            "position": [-6.105, -1.535, 0],
+            "rotation": {
+                "x": 0,
+                "y": Math.PI / 2,
+            },
+        }
+    ],
+    "Reminiscence": [],
+};
+
+function ShabbatTV(scene, id, url) {
+    const book = SETTINGS[id];
+
+    const source = document.getElementById(book.source_id);
+    source.src = url;
+
+    const video = document.getElementById(book.video_id);
+    video.load();
+    video.play();
+
+
+    function setup() {
+        // Calculate aspect ratio of the video
+        const aspectRatio = video.videoWidth / video.videoHeight;
+
+        // Set the height to fit the container height
+        let heightToFit = book.height;
+
+        // Calculate the width to fit the height without empty areas
+        let widthToFit = heightToFit * aspectRatio;
+
+        const texture = new THREE.VideoTexture(video);
+        texture.minFilter = THREE.LinearFilter;
+        texture.magFilter = THREE.LinearFilter;
+        //texture.format = THREE.RGBAFormat;
+
+        const material = book.material(texture);;
+
+        const geometry = new THREE.PlaneGeometry(widthToFit, heightToFit);
+
+        const screen = new THREE.Mesh(geometry, material);
+        screen.rotation.x = book.rotation.x || 0;
+        screen.rotation.y = book.rotation.y || 0;
+        screen.rotation.z = book.rotation.z || 0;
+        screen.position.set(...book.position);
+
+        scene.add(screen);
+
+        return screen;
+    }
+
+    // If video metadata is already loaded, setup immediately
+    if (video.readyState >= video.HAVE_METADATA) {
+        return setup();
+    }
+
+    // Otherwise, setup after the metadata is loaded
+    return new Promise(resolve => {
+        video.addEventListener("loadedmetadata", function() {
+            const screen = setup();
+            resolve(screen);
+        });
+    });
+}
+
+async function PravdaTV(scene, MEDIA) {
+    return;
+
+    return new Promise((resolve, reject) => {
+        const image = document.getElementById("image" + door.id);
+        image.src = image_url;
+
+        image.onload = function() {
+            try {
+                // Calculate aspect ratio of the image
+                const aspectRatio = image.naturalWidth / image.naturalHeight;
+
+                let heightToFit = door.height;
+                let widthToFit = heightToFit * aspectRatio;
+
+                // Create texture and material
+                const texture = new THREE.Texture(image);
+                texture.needsUpdate = true; // Important to update the texture after loading
+                const material = new THREE.MeshBasicMaterial({ map: texture, side: THREE.DoubleSide,  transparent: true });
+
+                // Create screen geometry
+                const geometry = new THREE.PlaneGeometry(widthToFit, heightToFit);
+
+                // Create and configure the screen mesh
+                const screen = new THREE.Mesh(geometry, material);
+                screen.rotation.x = door.rotation.x || 0;
+                screen.rotation.y = door.rotation.y || 0;
+                screen.rotation.z = door.rotation.z || 0;
+                screen.position.set(...door.position);
+
+                // Add the screen to the scene
+                scene.add(screen);
+
+                // Resolve the Promise with the created screen
+                resolve(screen);
+            } catch (error) {
+                // Reject the Promise if there's an error
+                reject(error);
+            }
+        };
+
+        image.onerror = function() {
+            // Reject the Promise if the image fails to load
+            reject(new Error("Failed to load image"));
+        };
+    });
+}
+
+
+async function MosesTV(scene) {
+    // TODO: Make this method customizable
+    scene.background = new THREE.Color( 0xf6eedc );
+
+    const GLTF = "assets/XXX/Teremok/scene.gltf";
+    const loader = new GLTFLoader();
+    loader.load(
+        GLTF,
+        function (gltf) {
+            // finish initialization
+					  const model = gltf.scene;
+					  model.scale.setScalar( 190 );
+					  model.position.set( 2, -5, 2);
+            scene.add(gltf.scene);
+        },
+        undefined,
+        function (error) {
+            console.error(error);
+        }
+    );
+
+}
+
+export {ShabbatTV, PravdaTV, MosesTV}
